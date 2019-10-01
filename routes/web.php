81b432fb11aaec65f635a43087f6c8a041493a93
Rus Hughes <russell.hughes@gmail.com>
2019-10-01 18:42:32 +0200

    s25 l174

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sendmail', function () {
+  $data = [
+    'title' => 'Hi this is an email subject not a title',
+    'content' => 'I guess this is the email body',
+  ];
+
+  Mail::send('emails.test', $data, function ($message) {
+    $message->to('rus@gmail.com', 'Rus Hughes')->subject('I owe Edwin a sorry!');
+  });
+});
